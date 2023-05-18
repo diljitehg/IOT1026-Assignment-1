2c80edb9e758428759708aeeabe55ae837f53f6e
diljitehg <131228332+diljitehg@users.noreply.github.com>
2023-05-18 15:12:32 -0400

    Assignment1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,64 +1,32 @@
-﻿namespace Assignment
+﻿using System;
+
+public class Program
 {
-    public static class ArrayReplicator
+    public static int AskForNumber(string text)
     {
-        /// <summary>
-        /// Replicates (deep copies) the incoming array
-        /// </summary>
-        /// <param name="original">The array to be replicated</param>
-        /// <returns>A deep copy of the original array</returns>
-        public static int[] ReplicateArray(int[] original)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Asks the user for a number. Will crash if the user input is not convertible to an int (throw exception?)
-        /// </summary>
-        /// <param name="text">Text to prompt the user</param>
-        /// <returns>The user input as an integer</returns>
-        public static int AskForNumber(string text)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Asks the user for a number within a certain range [min, max]. If the number is not in the range, re-prompt the user for a new number.
-        /// Will crash if the user input is not convertible to an int (throw exception?)
-        /// </summary>
-        /// <param name="text">Text to prompt the user</param>
-        /// <param name="min">Smallest permissible value</param>
-        /// <param name="max">Largest permissible value</param>
-        /// <returns>The user input as an integer</returns>
-        public static int AskForNumberInRange(string text, int min, int max)
-        {
-            throw new NotImplementedException();
-        }
+        Console.WriteLine(text);
+        string response = Console.ReadLine();
+        int number = int.Parse(response);
+        return number;
     }
 
-    static class Program
+    public static int AskForNumberInRange(string text, int min, int max)
     {
-        static void Main()
+        int number;
+        do
         {
-            /*
-            const int Min = 0;
-            const int Max = 10;
-            const int PrintOffset = 4;
+            number = AskForNumber(text);
+        } while (number < min || number > max);
 
-            int size = ArrayReplicator.AskForNumberInRange("Enter the array size: ", Min, Max);
-            int[] original = new int[size];
+        return number;
+    }
 
-            // Fill the original array with user specified integers
-            for (int item = 0; item < size; ++item)
-            {
-                original[item] = ArrayReplicator.AskForNumber("Enter a number: ");
-            }
+    public static void Main()
+    {
+        int number = AskForNumber("Enter a number: ");
+        Console.WriteLine("You entered: " + number);
 
-            int[] copy = ArrayReplicator.ReplicateArray(original);
-            // Verify original and replicated array are the same
-            for (int index = 0; index < size; ++index)
-                Console.WriteLine($"Original {original[index],-PrintOffset}  {copy[index],4} Copy");
-            */
-        }
+        int numberInRange = AskForNumberInRange("Enter a number between 1 and 10: ", 1, 10);
+        Console.WriteLine("You entered a number within the range: " + numberInRange);
     }
 }

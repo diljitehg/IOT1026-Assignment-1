ea6e0b2a839b519cef4f772d814e79f0685d1dc8
diljitehg <131228332+diljitehg@users.noreply.github.com>
2023-05-26 00:16:30 -0400

    Update Program.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,46 +1,172 @@
-﻿using System;
+using System;
 
-class Program
+namespace Assignment
 {
-    static int AskForNumber(string text)
+    public class TreasureChest
     {
-        Console.WriteLine(text);
-        string userInput = Console.ReadLine();
-        int number = int.Parse(userInput);
-        return number;
-    }
+        private State _state = State.Locked;
+        private readonly Material _material;
+        private readonly LockType _lockType;
+        private readonly LootQuality _lootQuality;
 
-    static int AskForNumberInRange(string text, int min, int max)
-    {
-        int number;
-        do
+        public TreasureChest()
         {
-            number = AskForNumber(text);
-        } while (number < min || number > max);
-
-        return number;
-    }
-
-    static int[] ReplicateArray(int[] original)
-    {
-        int[] copiedArray = new int[original.Length];
-        Array.Copy(original, copiedArray, original.Length);
-        return copiedArray;
-    }
-
-    static void Main()
-    {
-        int size = AskForNumberInRange("Enter the size of the array (0 to 5): ", 0, 5);
-        int[] originalArray = new int[size];
-
-        for (int i = 0; i < size; i++)
-        {
-            originalArray[i] = AskForNumber("Enter an integer: ");
+            _material = Material.Iron;
+            _lockType = LockType.Expert;
+            _lootQuality = LootQuality.Green;
         }
 
-        int[] copiedArray = ReplicateArray(originalArray);
+        public TreasureChest(State state) : this()
+        {
+            _state = state;
+        }
 
-        Console.WriteLine("Original Array: " + string.Join(", ", originalArray));
-        Console.WriteLine("Copied Array: " + string.Join(", ", copiedArray));
+        public TreasureChest(Material material, LockType lockType, LootQuality lootQuality)
+        {
+            _material = material;
+            _lockType = lockType;
+            _lootQuality = lootQuality;
+        }
+
+        public State GetState()
+        {
+            return _state;
+        }
+
+        public void Manipulate(Action action)
+        {
+            switch (action)
+            {
+                case Action.Open:
+                    Open();
+                    break;
+                case Action.Close:
+                    Close();
+                    break;
+                case Action.Lock:
+                    Lock();
+                    break;
+                case Action.Unlock:
+                    Unlock();
+                    break;
+            }
+        }
+
+        private void Unlock()
+        {
+            if (_state == State.Locked)
+            {
+                _state = State.Closed;
+            }
+            else if (_state == State.Closed)
+            {
+                Console.WriteLine("The chest is already unlocked.");
+            }
+            else if (_state == State.Open)
+            {
+                Console.WriteLine("The chest cannot be unlocked because it is already open.");
+            }
+        }
+
+        private void Lock()
+        {
+            if (_state == State.Closed)
+            {
+                _state = State.Locked;
+            }
+            else if (_state == State.Locked)
+            {
+                Console.WriteLine("The chest is already locked.");
+            }
+            else if (_state == State.Open)
+            {
+                Console.WriteLine("The chest cannot be locked because it is already open.");
+            }
+        }
+
+        private void Open()
+        {
+            if (_state == State.Closed)
+            {
+                _state = State.Open;
+            }
+            else if (_state == State.Open)
+            {
+                Console.WriteLine("The chest is already open.");
+            }
+            else if (_state == State.Locked)
+            {
+                Console.WriteLine("The chest cannot be opened because it is locked.");
+            }
+        }
+
+        private void Close()
+        {
+            if (_state == State.Open)
+            {
+                _state = State.Closed;
+            }
+            else if (_state == State.Closed)
+            {
+                Console.WriteLine("The chest is already closed.");
+            }
+            else if (_state == State.Locked)
+            {
+                Console.WriteLine("The chest cannot be closed because it is locked.");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"A {_state} chest with the following properties:\nMaterial: {_material}\nLock Type: {_lockType}\nLoot Quality: {_lootQuality}";
+        }
+
+        public enum State { Open, Closed, Locked };
+        public enum Action { Open, Close, Lock, Unlock };
+        public enum Material { Oak, RichMahogany, Iron };
+        public enum LockType { Novice, Intermediate, Expert };
+        public enum LootQuality { Grey, Green, Purple };
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TreasureChest chest = new TreasureChest();
+
+            Console.WriteLine(chest); // Output: A Locked chest with the following properties: Material: Iron, Lock Type: Expert, Loot Quality: Green
+
+            chest.Manipulate(TreasureChest.Action.Open);
+            Console.WriteLine(chest.GetState()); // Output: Open
+
+            chest.Manipulate(TreasureChest.Action.Close);
+            Console.WriteLine(chest.GetState()); // Output: Closed
+
+            chest.Manipulate(TreasureChest.Action.Lock);
+            Console.WriteLine(chest.GetState()); // Output: Locked
+
+            chest.Manipulate(TreasureChest.Action.Unlock);
+            Console.WriteLine(chest.GetState()); // Output: Closed
+
+            chest.Manipulate(TreasureChest.Action.Open);
+            Console.WriteLine(chest.GetState()); // Output: Open
+
+            chest.Manipulate(TreasureChest.Action.Lock);
+            Console.WriteLine(chest.GetState()); // Output: Open (cannot be locked because it is already open)
+
+            chest.Manipulate(TreasureChest.Action.Close);
+            Console.WriteLine(chest.GetState()); // Output: Closed
+
+            chest.Manipulate(TreasureChest.Action.Unlock);
+            Console.WriteLine(chest.GetState()); // Output: Closed (cannot be unlocked because it is already closed)
+
+            chest.Manipulate(TreasureChest.Action.Open);
+            Console.WriteLine(chest.GetState()); // Output: Open
+
+            chest.Manipulate(TreasureChest.Action.Lock);
+            Console.WriteLine(chest.GetState()); // Output: Locked
+
+            Console.WriteLine(chest); // Output: A Locked chest with the following properties: Material: Iron, Lock Type: Expert, Loot Quality: Green
+        }
     }
 }
